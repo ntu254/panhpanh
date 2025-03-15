d8b6e4c61a1838b4db4ba11f91bc690d645fbc1a
ntu254 <nttu254.vn@gmail.com>
2025-03-15 21:36:07 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
+const CONFIG = {
+    titleWeb: "💖 Cute Interaction 💖",
+    title: "Bạn có thích mình không? 💖",
+    desc: "Hãy chọn 'Có' hoặc 'Không' nhé! 😊",
+    btnYes: "Có 💖",
+    btnNo: "Không 😢",
+    introTitle: "Xin chào! 💖",
+    introDesc: "Mình có điều này muốn hỏi bạn, bạn hãy trả lời thật lòng nhé!",
+    btnIntro: "Tiếp tục 🎀",
+    question: "Bạn có thích mình không? 💖",
+    reply: "Tại vì bạn quá dễ thương! 😊",
+    mess: "Mình biết mà! 💖",
+    messDesc: "Hãy cùng nhau trò chuyện nhé!",
+    btnAccept: "Đồng ý 🎀",
+    messLink: "https://www.facebook.com"
+};
+
 $(document).ready(function() {
-    // process bar
+    // Preloader
     setTimeout(function() {
         firstQuestion();
         $('.spinner').fadeOut();
         $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
+        $('body').delay(350).css({ 'overflow': 'visible' });
     }, 600);
-})
+});
 
 function init() {
-    document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb
-    $('#title').text(CONFIG.title)
-    $('#desc').text(CONFIG.desc)
-    $('#yes').text(CONFIG.btnYes)
-    $('#no').text(CONFIG.btnNo)
+    document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb;
+    $('#title').text(CONFIG.title);
+    $('#desc').text(CONFIG.desc);
+    $('#yes').text(CONFIG.btnYes);
+    $('#no').text(CONFIG.btnNo);
 
     var xYes = (0.9 * $(window).width() - $('#yes').width() - $('#no').width()) / 2;
     var xNo = xYes + $('#yes').width() + 0.1 * $(window).width();
     var y = 0.75 * $(window).height();
     $('#yes').css("left", xYes);
     $('#yes').css("top", y);
-
     $('#no').css("left", xNo);
     $('#no').css("top", y);
 }
@@ -32,22 +46,21 @@ function firstQuestion() {
     Swal.fire({
         title: CONFIG.introTitle,
         text: CONFIG.introDesc,
-        imageUrl: 'img/logi.gif',
-        imageWidth: 300,
-        imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
-        imageAlt: 'Custom image',
+        imageUrl: 'img/cute-heart.gif',
+        imageWidth: 200,
+        imageHeight: 200,
+        background: '#fff url("img/cute-background.jpg")',
         confirmButtonText: CONFIG.btnIntro
     }).then(function() {
         $('.content').show(200);
-        var audio = new Audio('sound/sound.mp3');
+        var audio = new Audio('sound/cute-sound.mp3');
         audio.play();
-    })
+    });
 }
 
-// switch button position
+// Switch button position
 function switchButton() {
-    var audio = new Audio('sound/duck.mp3');
+    var audio = new Audio('sound/cute-switch.mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
@@ -58,9 +71,10 @@ function switchButton() {
     $('#yes').css("left", leftNo);
     $('#yes').css("top", topNO);
 }
-// move random button position
+
+// Move button randomly
 function moveButton() {
-    var audio = new Audio('sound/Swish1.mp3');
+    var audio = new Audio('sound/cute-move.mp3');
     audio.play();
     var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9;
     var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
@@ -70,75 +84,48 @@ function moveButton() {
     $('#no').css("top", top);
 }
 
-init()
+init();
 
 var n = 0;
 $('#no').mousemove(function() {
-    if (Math.random() < 0.5 || n == 1)
-        switchButton();
-    else
-        moveButton();
+    if (Math.random() < 0.5 || n == 1) switchButton();
+    else moveButton();
     n++;
 });
 $('#no').click(() => {
-    if (screen.width >= 900)
-        switchButton();
-})
+    if (screen.width >= 900) switchButton();
+});
 
-// generate text in input
-function textGenerate() {
-    var n = "";
-    var text = " " + CONFIG.reply;
-    var a = Array.from(text);
-    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
-    var count = textVal.length;
-    if (count > 0) {
-        for (let i = 1; i <= count; i++) {
-            n = n + a[i];
-            if (i == text.length + 1) {
-                $('#txtReason').val("");
-                n = "";
-                break;
-            }
-        }
-    }
-    $('#txtReason').val(n);
-    setTimeout("textGenerate()", 1);
-}
-
-// show popup
+// Show popup when clicking "Yes"
 $('#yes').click(function() {
-    var audio = new Audio('sound/tick.mp3');
+    var audio = new Audio('sound/cute-tick.mp3');
     audio.play();
     Swal.fire({
         title: CONFIG.question,
-        html: true,
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate() placeholder='Tại sao bạn thích mình?'>",
         width: 900,
         padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/cute-background.jpg")',
         backdrop: `
-              rgba(0,0,123,0.4)
-              url("img/giphy2.gif")
-              left top
-              no-repeat
-            `,
-        confirmButtonColor: '#3085d6',
-        confirmButtonColor: '#fe8a71',
+            rgba(255, 182, 193, 0.4)
+            url("img/cute-gif.gif")
+            left top
+            no-repeat
+        `,
         confirmButtonText: CONFIG.btnReply
     }).then((result) => {
         if (result.value) {
             Swal.fire({
                 width: 900,
                 confirmButtonText: CONFIG.btnAccept,
-                background: '#fff url("img/iput-bg.jpg")',
+                background: '#fff url("img/cute-background.jpg")',
                 title: CONFIG.mess,
                 text: CONFIG.messDesc,
-                confirmButtonColor: '#83d0c9',
+                confirmButtonColor: '#ff6f61',
                 onClose: () => {
                     window.location = CONFIG.messLink;
                 }
-            })
+            });
         }
-    })
-})
+    });
+});

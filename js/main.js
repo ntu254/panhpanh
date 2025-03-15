e917f849d5e87d250b5d40cff83fd0a4bbf83560
ntu254 <nttu254.vn@gmail.com>
2025-03-15 21:38:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 const CONFIG = {
-    titleWeb: "💖 Cute Interaction 💖",
-    title: "Bạn có thích mình không? 💖",
-    desc: "Hãy chọn 'Có' hoặc 'Không' nhé! 😊",
-    btnYes: "Có 💖",
-    btnNo: "Không 😢",
-    introTitle: "Xin chào! 💖",
-    introDesc: "Mình có điều này muốn hỏi bạn, bạn hãy trả lời thật lòng nhé!",
-    btnIntro: "Tiếp tục 🎀",
-    question: "Bạn có thích mình không? 💖",
-    reply: "Tại vì bạn quá dễ thương! 😊",
-    mess: "Mình biết mà! 💖",
-    messDesc: "Hãy cùng nhau trò chuyện nhé!",
-    btnAccept: "Đồng ý 🎀",
-    messLink: "https://www.facebook.com"
+    titleWeb: "Panh Panh",
+    introTitle: 'Panh',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã mê ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại gặp được anh <3',
+    btnReply: 'Gửi cho u <3',
+    reply: 'Anh Tú siêu cấp vũ trụ đẹp zai đáng yêu tốt bụng <33333333',
+    mess: 'Anh biết mà 🥰. Em cũng dị đóa nha 😘😘',
+    messDesc: 'Cảm ơn ông Trân vì anh đã gặp đc em',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/28685511487763644/#' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 };
 
 $(document).ready(function() {

1dbde0410cf57b912c761c5bafd7ca88b620512c
ntu254 <nttu254.vn@gmail.com>
2025-03-15 20:58:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Panh Panh",
+    introTitle: 'Panh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Phải chăng em đã mê ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại gặp được anh <3',
+    btnReply: 'Gửi cho u <3',
+    reply: 'Anh Tú siêu cấp vũ trụ đẹp zai đáng yêu tốt bụng <33333333',
+    mess: 'Anh biết mà 🥰. Em cũng dị đóa nha 😘😘',
+    messDesc: 'Cảm ơn ông Trân vì anh đã gặp đc em',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/e2ee/t/28685511487763644/#' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
